523f05677fc440fd8ba3928dc059731bc8a9cb63
Muhammad-Bilal300 <bilalaarbi300@gmail.com>
2024-07-29 15:58:12 +0500

    code

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Code Updated in dev again
+          Code Updated in main
         </p>
         <a
           className="App-link"
